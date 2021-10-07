c5d0327360694cb6471c63f6a8928ab2d2994794
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-10-07 17:08:13 -0300

    consumindo api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+
 export function Home() {
+
+    const [listHeroe, setListHeroe] = useState([])
+
+    useEffect(() => {
+        api.get('all.json')
+            .then((response) => {
+                setListHeroe(response.data)
+            }).catch(() => {
+                console.log("Api Incorreta")
+            })
+    }, [])
+
     return (
-        <h1>Tela Inicial</h1>
+        <div>
+            {listHeroe.map((heroe, key) => {
+                return (
+                    <div key={key}>
+                        <img src={heroe.images.sm} />
+                        <h2>{heroe.name}</h2>
+                        <strong>Raça:</strong> <p>{heroe.appearance.race}</p>
+                        <strong>Nome:</strong> <p>{heroe.biography.fullName}</p>
+                        <strong>Editora:</strong> <p>{heroe.biography.publisher}</p>
+                    </div>
+                )
+            })}
+
+        </div>
     )
 }
